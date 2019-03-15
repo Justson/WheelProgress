f1771c39e38781e41b579afaeff320cd2a9f1117
ringle-android <xiaozhongcen@gmail.com>
2019-03-15 14:11:43 +0800

    1.优化代码

--- library/src/main/java/com/wheelloading/library/WheelProgress.java
+++ library/src/main/java/com/wheelloading/library/WheelProgress.java
@@ -9,7 +9,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.animation.LinearInterpolator;
@@ -36,25 +35,46 @@ public class WheelProgress extends View {
      * 画笔
      */
     Paint paint = null;
-
-    //控件的宽和高
-    private int WHEELWIDTH = 80;
-    private int WHEELHEIGHT = 80;
-    //用于计算屏幕的密度
+    /**
+     * 控件的宽和高
+     */
+    private int WHEEL_WIDTH = 80;
+    /**
+     * 控件的宽和高
+     */
+    private int WHEEL_HEIGHT = 80;
+    /**
+     * 用于计算屏幕的密度
+     */
     private static int DENSITY;
-
-    //控件的实际宽高
+    /**
+     * 控件的实际宽高
+     */
     private int resultHeight;
     private int resultWidth;
-    //用于绘制条目
+    /**
+     * 用于绘制条目
+     */
     private static final RectF rectF = new RectF();
-    //用于控制透明度，默认为完全不透明
+    /**
+     * 用于控制透明度，默认为完全不透明
+     */
     private static final int ALPHA = 255;
-    //条目数量
+    /**
+     * 条目数量
+     */
     private static final int itemNumber = 12;
+    /**
+     * 圆心位置
+     */
     private float centerX;
+    /**
+     * 圆心位置
+     */
     private float centerY;
-    //速度因子
+    /**
+     * 速度因子
+     */
     private int speed = 100;
     /**
      * 是否旋转
@@ -87,14 +107,10 @@ public class WheelProgress extends View {
     }
 
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-        itemColor = Color.WHITE; //默认为白色
-
+        itemColor = Color.WHITE;
         DENSITY = (int) context.getResources().getDisplayMetrics().density; //获取屏幕的密度
-
-        WHEELHEIGHT *= DENSITY;
-        WHEELWIDTH *= DENSITY;
-
-
+        WHEEL_HEIGHT *= DENSITY;
+        WHEEL_WIDTH *= DENSITY;
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.WheelProgress, defStyleAttr, 0);
         int count = ta.getIndexCount();//获取自定义所有属性个数
         for (int i = 0; i < count; i++) {
@@ -105,19 +121,13 @@ public class WheelProgress extends View {
                 //获取自定义条目个数
             } else if (index == R.styleable.WheelProgress_itemWidth) {
                 itemWidth = ta.getInteger(index, 12);
-
             } else if (index == R.styleable.WheelProgress_itemCornersRadius) {
                 itemCornersRadius = ta.getFloat(index, 5f);
-
-                //获取Boolean 属性
             } else if (index == R.styleable.WheelProgress_oneSeftRotate) {
                 oneSeftRotate = ta.getBoolean(index, false);
-                Log.i("Info", "soneSeftRotate:" + oneSeftRotate);
-
             }
         }
         ta.recycle();
-        //初始化画笔
         paint = new Paint();
         paint.setColor(itemColor);
         paint.setStyle(Paint.Style.FILL);
@@ -125,9 +135,7 @@ public class WheelProgress extends View {
         paint.setDither(true);
         paint.setStrokeCap(Paint.Cap.ROUND);
         paint.setAlpha(255);
-
         this.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-
             @Override
             public void onGlobalLayout() {
                 resultWidth = WheelProgress.this.getMeasuredWidth();
@@ -153,7 +161,7 @@ public class WheelProgress extends View {
         }
     }
 
-    @Override //取消属性动画
+    @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         if (objectAnimator != null && objectAnimator.isStarted()) {
@@ -161,57 +169,57 @@ public class WheelProgress extends View {
         }
     }
 
-    //初始化要绘制条目 RectF  属性
+    /**
+     * 初始化要绘制条目 RectF 属性
+     */
     public void initRectFProperty() {
-
         int radius = resultWidth / 2;
         //记录圆心的位置， 用于旋转canvas
         centerX = radius;
         centerY = radius;
-
         int startPosition = (radius) - itemWidth / 2;
-
         int lineLength = (radius) / 2 - 5;
-
         rectF.left = startPosition;
-
         rectF.top = 0 + 15f;
-
         rectF.right = startPosition + itemWidth;
-
         rectF.bottom = rectF.top + lineLength;
     }
 
-    @Override  //测量该控件
+    /**
+     * 测量控件
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
         int width = MeasureSpec.getSize(widthMeasureSpec);
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int height = MeasureSpec.getSize(heightMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
         width = height = Math.min(width, height);     //两者之间取最小值
-        this.setMeasuredDimension(widthMode == MeasureSpec.AT_MOST ? WHEELWIDTH : width, heightMode == MeasureSpec.AT_MOST ? WHEELHEIGHT : height);
+        this.setMeasuredDimension(widthMode == MeasureSpec.AT_MOST ? WHEEL_WIDTH : width, heightMode == MeasureSpec.AT_MOST ? WHEEL_HEIGHT : height);
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
-
-        canvas.rotate(controler * 30f, centerX, centerY); //旋转画布
+        /**
+         * 旋转画布
+         */
+        canvas.rotate(controler * 30f, centerX, centerY);
         controler++;
         if (controler == 12) {
             controler = 0;
         }
         for (int i = 0; i < itemNumber; i++) {
-
+            /**
+             * 设置透明度
+             */
             canvas.drawRoundRect(rectF, itemCornersRadius, itemCornersRadius, paint);
-
-            int tem = ALPHA - ((ALPHA / itemNumber) * i); //设置透明度
-
+            int tem = ALPHA - ((ALPHA / itemNumber) * i);
             if (tem <= 80) {
                 tem = 80;
             }
-
             paint.setAlpha(tem);
             canvas.rotate(-30f, centerX, centerY);
         }
